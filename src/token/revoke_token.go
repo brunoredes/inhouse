a88dc0c250f88613eb223631cac6723680d8a9eb
Bruno Donatelli <bruno.donatelli17@gmail.com>
2025-02-21 15:40:53 -0300

    feat(revoke-token): retrieve revoked token

--- src/token/revoke_token.go
+++ src/token/revoke_token.go
@@ -7,11 +7,16 @@ import (
 
 // Store revoked tokens in Redis (for a short duration)
 func RevokeInhouseToken(tokenString string) error {
-	return database.RedisClient.Set(database.Ctx, "revoked:"+tokenString, "true", 2*time.Hour).Err()
+	return database.RedisClient.Set(database.Ctx, "revoked:"+tokenString, "true", 10*time.Hour).Err()
+}
+
+func GetRevokedToken(tokenString string) (string, error) {
+	return database.RedisClient.Get(database.Ctx, "revoked:"+tokenString).Result()
+
 }
 
 // Check if a token is revoked
 func IsTokenRevoked(tokenString string) bool {
-	val, err := database.RedisClient.Get(database.Ctx, "revoked:"+tokenString).Result()
+	val, err := GetRevokedToken(tokenString)
 	return err == nil && val == "true"
 }
